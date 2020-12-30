680de2f8e3b1e443df6d7ff97402b4618c2fb06d
m-codes <martinwalsh.dev@gmail.com>
2020-12-30 18:40:40 +0000

    Move object, launch ball, prefabs and object destruction added

---
+++ BubblePop/Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config params
+    [SerializeField] FishMove fish1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
+
+
+    //State
+    Vector2 fishToBallVector;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishToBallVector = transform.position - fish1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPanel();
+            LaunchBallOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPanel()
+    {
+        Vector2 fishPos = new Vector2(fish1.transform.position.x, fish1.transform.position.y);
+        transform.position = fishPos + fishToBallVector;
+    }
+}
---
+++ BubblePop/Assets/Scripts/FishMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    //Config parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    //Edges of screen for sprite
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Current mouse position
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        // Current fish sprite position
+        Vector2 fishPos = new Vector2(transform.position.x, transform.position.y);
+        // Set fish x positon to the mouse position (If within the screen size)
+        fishPos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = fishPos;
+        Debug.Log(mousePosInUnits);
+    }
+}
